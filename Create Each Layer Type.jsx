785167a6f4a81d7ed687d7de34825f369a36bc1c
Nate Lovell <Natelovell99@gmail.com>
2020-07-06 04:08:43 -0600

    Create Create Each Layer Type.jsx

---
+++ Create Each Layer Type.jsx
@@ -0,0 +1,18 @@
+﻿// create each layer type
+app.beginUndoGroup("Layers Creation");
+
+var comp = app.project.activeItem;
+
+var nullLayer = comp.layers.addNull(app.project.activeItem.duration);
+
+var solidLayer = comp.layers.addSolid([1, 1, 1], "Solid Layer", 500, 500, 1, comp.duration);
+
+var textLayer = comp.layers.addText("Textttt");
+
+var cameraLayer = comp.layers.addCamera("Camera 1", [comp.width/2, comp.height/2]);
+
+var lightLayer = comp.layers.addLight("Light 2", [comp.width/2, comp.height/2]);
+
+var shapeLayer = comp.layers.addShape();
+
+app.endUndoGroup();
